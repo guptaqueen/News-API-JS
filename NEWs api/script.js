1813dba46682d01cc47622f727fbe567f2ddc7ab
Manisha Gupta <rouniyarmanisha@gmail.com>
2025-03-04 11:59:22 +0545

    first

---
+++ NEWs api/script.js
@@ -0,0 +1,11 @@
+const apikey = 'b82c3ea567c2408ab092b67a3296d567'
+const blogContainer = document.getElementsByClassName("blog-container");
+
+function fetchRandomNews(){
+  try{
+
+  }catch(error){
+    console.error("error fetching random news",error)
+return[]
+  }
+}
